958589db90b892c51c64713bddb650bd7a97423a
nabeel khalaf <raed.1142118@gmail.com>
2019-09-10 14:11:39 +0300

    Support different delimiters to be customized in the beginning of the equation

--- SimpleCalculator/Program.fs
+++ SimpleCalculator/Program.fs
@@ -2,20 +2,44 @@
 open System.Text.RegularExpressions
 
 
-let splitNumbers(equation: String) : String[]=
-    (* Split the number based on , and \n delimiter *)
-    let mutable it : String [] = [|",";"\\n"|] 
-    equation.Split(it,StringSplitOptions.RemoveEmptyEntries)
+let getNumbersFromEquation(equation: String)=
+    (*Get the numbers from the equation
+      Example //;\n1,2;3,4  will return 1,2;3,4  
+     *)
+    let index = System.Text.RegularExpressions.Regex.Match(equation,"\d").Index
+    equation.[index..]
+let splitNumbers(equation: String, delimiters: String[]) : String[]=
+    (* Split the number based on specified delimiters and return array*)
+    equation.Split(delimiters,StringSplitOptions.RemoveEmptyEntries)
     
-let summation(number1: int , number2: int) :int = number1 + number2   
-
-let processEquation(equation: String ) :int =
-    (* Convert List of String numbers to integers and get summation *)
-    let numbers = splitNumbers(equation)
+let summation(numbers: String[])=
+    (* Calculate the summations of all numbers *)
     let mutable  result = 0 
     for number in numbers do
-        result <- summation(result,Int32.Parse(number))
-    result    
+        result <- result + Int32.Parse(number)
+    result
+    
+let ParseDelimitersFromEquationLine(equation: String): String[]=
+     (* Get the Custom delimiters specified in the equation
+     Example //;\n  ";" will be returned *)
+     [|equation.[2..2]|]
+    
+let getDelimiters(equation: String)=
+    (*Combine the default delimiters ',' and '\n' with the custom  *)
+    let defaultDelimiters : String [] = [|",";"\\n"|]
+    let mutable customDelimiters: String[] = [||]
+    if equation.Chars(0) = '/' then
+        customDelimiters <- ParseDelimitersFromEquationLine(equation)
+    Array.concat [ customDelimiters ; defaultDelimiters ]
+    
+let processEquation(equation: String ) :int =
+    (* Convert List of String numbers to integers and get summation *)
+    
+    let delimiters = getDelimiters(equation)
+    let equationNumbers = getNumbersFromEquation(equation)
+    let parsedNumbers = splitNumbers(equationNumbers, delimiters)
+    let result = summation(parsedNumbers)
+    result
             
 let calculateResults(equation: String)=
      (* Check if the equation is empty return 0 if not process the equation
@@ -28,7 +52,7 @@ let calculateResults(equation: String)=
 
 [<EntryPoint>]
 let main argv =
-    let equation  = Console.ReadLine()
+    let mutable equation  = Console.ReadLine()
     let sumResult= calculateResults(equation)
     printfn "summation  is %d" sumResult
     Console.ReadKey() |> ignore 
