027279ca49fa60ca27182779195923fa4bd1d27d
nabeel khalaf <raed.1142118@gmail.com>
2019-09-10 11:34:51 +0300

    Support calculator to handle new lines between numbers, and fix some naming convention

--- SimpleCalculator/Program.fs
+++ SimpleCalculator/Program.fs
@@ -1,33 +1,35 @@
 ﻿open System
+open System.Text.RegularExpressions
 
 
-let split_numbers(equation: String) : String[]=
-    (* Split the number based on , delimiter *)
-    equation.Split(',')
+let splitNumbers(equation: String) : String[]=
+    (* Split the number based on , and \n delimiter *)
+    let mutable it : String [] = [|",";"\\n"|] 
+    equation.Split(it,StringSplitOptions.RemoveEmptyEntries)
     
 let summation(number1: int , number2: int) :int = number1 + number2   
 
-let process_equation(equation: String ) :int =
+let processEquation(equation: String ) :int =
     (* Convert List of String numbers to integers and get summation *)
-    let numbers = split_numbers(equation)
+    let numbers = splitNumbers(equation)
     let mutable  result = 0 
     for number in numbers do
         result <- summation(result,Int32.Parse(number))
     result    
             
-let calculate_results(equation: String)=
+let calculateResults(equation: String)=
      (* Check if the equation is empty return 0 if not process the equation
      equation : string that contain the equation to calculate
      return sum: int fot the summation results 
      *)
      match equation.Length with
     | 0  ->  0  // return 0 if the equation is empty 
-    | _ -> process_equation(equation)
+    | _ -> processEquation(equation)
 
 [<EntryPoint>]
 let main argv =
     let equation  = Console.ReadLine()
-    let sumResult= calculate_results(equation)
+    let sumResult= calculateResults(equation)
     printfn "summation  is %d" sumResult
     Console.ReadKey() |> ignore 
     0 // return an integer exit code
